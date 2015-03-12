fc94583a4ac19f1a096bcda7d34effc113dcf314
Anurag Vaishwade <ranurag@zeomega.com>
2015-03-12 18:16:53 +0530

    Changed nex palindrome from bruetforce

--- palin_the_next_palindrome.py
+++ palin_the_next_palindrome.py
@@ -1,8 +1,38 @@
+import re
 t = input()
 for i  in range(t):
     num = input()
-    while True:
-        num += 1
-        if str(num) == str(num)[::-1]:
-            print num
-            break
+    snum = str(num)
+    if len(snum) == 1:
+        print num+1
+    if snum == snum[::-1]:
+        if not re.match(r"[0-8]", snum):
+            print num+2
+            continue
+        if len(snum)%2 == 0:
+            place = str(long(snum[:len(snum)/2])+1)
+            print place+place[::-1]
+            continue
+        else:
+            left = snum[:len(snum)/2]
+            right = snum[len(snum)/2:]
+            if right[0] == '9':
+                rplace = str(long(right[::-1])+1)[::-1]
+                left = list(left)
+                right = list(right)
+                left[-1]=rplace[-1]
+                right[0]=rplace[0]
+                right[1]=rplace[1]
+                print "".join(left+right)
+                continue
+            else:
+                rplace = str(long(right[::-1])+1)[::-1]
+                print left+rplace
+                continue
+    else:
+        if len(snum)%2 == 0:
+             place = str(long(snum[:len(snum)/2])+1)
+             print place + place[::-1]
+        else:
+             place = str(long(snum[:len(snum)/2+1])+1)
+             print place + place[1::-1]
